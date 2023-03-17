527557632cd5745871e41e9dc6fc750c7d94df03
Matt <126122622+Q4OS-Wolferus@users.noreply.github.com>
2023-03-17 00:45:46 +0000

    import random
    def generate_seed():
        """
        Generate a random Minecraft world seed for version 1.19.4
        """
        return random.randint(-2**63, 2**63-1)
    
    seed = generate_seed()
    
    print(f"Your Minecraft world seed is: {seed}")
    
    it outputs 2 minecraft seeds for your minecraft worlds!

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import random
 
 def generate_seed():
     """
-    Generate a random Minecraft world seed for version 1.17.1.
+    Generate a random Minecraft world seed for version 1.19.4
+    """
+    return random.randint(-2**63, 2**63-1)
+
+seed = generate_seed()
+
+print(f"Your Minecraft world seed is: {seed}")
+import random
+
+def generate_seed():
+    """
+    Generate a random Minecraft world seed for version 1.19.4
     """
     return random.randint(-2**63, 2**63-1)
 
